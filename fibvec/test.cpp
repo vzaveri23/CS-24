de746cda922fb8a024d92ba7197738639f85d5dd
vzaveri23 <virajsz1123@gmail.com>
2022-10-06 10:36:10 -0700

    intial commit of test.cpp

--- fibvec/test.cpp
+++ fibvec/test.cpp
@@ -1,13 +1,14 @@
 #include "FibVec.h"
 #include <iostream>
+using namespace std;
 
 // Use this file to test your FibVec class!
 // This file won't be graded - do whatever you want.
 
 int main() {
   FibVec fibvec;
-
-  std::cout << "This program doesn't do anything interesting yet...\n";
+  
+  cout << "This program doesn't do anything interesting yet...\n";
 
   return 0;
 }
