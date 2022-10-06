7603461ff1576b159b6caf5e6438a13502a02660
vzaveri23 <virajsz1123@gmail.com>
2022-10-06 10:32:38 -0700

    intial commit of FibVec.h

---
+++ fibvec/FibVec.h
@@ -0,0 +1,26 @@
+//FibVec.h
+#ifndef FIBVEC_H_
+#define FIBVEC_H_
+#include <cstddef>
+using namespace std;
+
+class FibVec {
+    private:
+       int *arr;
+       size_t numVals;
+       size_t prev_cap;
+       size_t current_cap;
+    public:
+        size_t capacity() const;
+        size_t count() const;
+        void insert(int, size_t) throw();
+        int lookup(size_t) const throw();
+        int pop() throw();
+        void push(int);
+        int remove(size_t) throw();
+        FibVec slice(size_t, size_t) const;
+        void resize(size_t);
+
+};
+
+#endif /* FIBVEC_H */
