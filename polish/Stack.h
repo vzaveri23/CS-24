5239947550939b43258ff412230c79a6989c2840
vzaveri23 <virajsz1123@gmail.com>
2022-10-27 11:42:18 -0700

    initial commit

--- polish/Stack.h
+++ polish/Stack.h
@@ -1,7 +1,32 @@
 #ifndef STACK_H
 #define STACK_H
 
+#include "AST.h"
+
 // Use this file to define your Stack type.
 // Implement its member functions in Stack.cpp.
+
+struct Node {
+    AST *data;
+    Node *next;
+};
+
+class Stack {
+    
+    Node *top;
+
+    private:
+        size_t operators;
+        size_t operands;
+    
+    public:
+        Stack();
+        ~Stack();
+        void push(AST *);
+        AST* pop();
+        bool isEmpty() const;
+        Node* topNode() const;  
+        size_t size() const;
+};
 
 #endif
