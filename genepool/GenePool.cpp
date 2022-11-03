7091c0a303374e3c9e504dfe0fb86ba576912deb
vzaveri23 <virajsz1123@gmail.com>
2022-11-03 10:35:25 -0700

    wrote parser

--- genepool/GenePool.cpp
+++ genepool/GenePool.cpp
@@ -1,3 +1,74 @@
 #include "GenePool.h"
+using namespace std;
+#include <iostream>
 
 // GenePool Member Functions
+
+GenePool::GenePool(istream &stream) {
+    string line;
+    while (getline(stream, line)) {
+        if (line[0] == '#' || line.empty()) {
+            continue;
+        }
+
+        istringstream iss(line);
+        string t;
+        size_t count=0;
+        string name, g, m, f;
+        Gender gender;
+        Person *mother;
+        Person *father;
+        while (getline(iss, t, '\t')) {
+            switch(count) {
+                case 0:
+                    name = t;
+                    break;
+                case 1:
+                    if (t == "male") {
+                        gender = Gender::MALE;
+                    } else {
+                        gender = Gender::FEMALE;
+                    }
+                    break;
+                case 2:
+                    if (t == "???") {
+                        mother = nullptr;
+                    } else {
+                        mother = find(t);
+                    }
+                    break;
+                case 3:
+                    if (t == "???") {
+                        father = nullptr;
+                    } else {
+                        father = find(t);
+                    }
+                    break;
+            }
+
+            count++;
+        }
+
+        Person *ptr = new Person(name, gender, mother, father);
+        people[name] = ptr;
+        if (mother) {
+            mother->addChild(ptr);
+        }
+
+        if (father) {
+            father->addChild(ptr);
+        }
+
+        cout << "current name: " << name << endl;
+    }
+}
+
+GenePool::~GenePool() {
+    for (auto [k, v] : people) {
+        delete v;
+    }
+}
+
+Person* GenePool::find(const string &name) const {
+    return (people.count(name)==0) ? nullptr : people.at(name);
+}
