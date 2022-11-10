2fb5608e3dde13bbc4b97bf7fba1f6eb99ee90e6
vzaveri23 <virajsz1123@gmail.com>
2022-11-10 10:52:27 -0800

    updated dictionary functions

--- typo/Dictionary.cpp
+++ typo/Dictionary.cpp
@@ -1,30 +1,23 @@
 #include "Dictionary.h"
 #include <cmath>
+#include <iostream>
 using namespace std;
 
 double distance(Point, Point);
 double score(double);
+bool isLower(string);
 
 Dictionary::Dictionary(istream &stream) {
     string line;
-    bool flag = true;
     while (getline(stream, line)) {
-        for (char c : line) {
-            if (!islower(c)) {
-                flag = false;
-                break;
-            }
-        }
-        
-        if (flag) {
+        if (isLower(line)) {
             mWords.push_back(line);
-        } 
+        }
     }
 }
 
 Heap Dictionary::correct(const vector<Point> &points, size_t maxcount, float cutoff) const {
     Heap h(maxcount);
-
     for (string s : mWords) {
         if (s.size() == points.size()) {
             double sc = 0;
@@ -33,17 +26,30 @@ Heap Dictionary::correct(const vector<Point> &points, size_t maxcount, float cut
                 sc += score(distance(p1, points[i]));
             }
 
-            if (sc/s.size() >= cutoff) {
+            if (h.count() == maxcount) {
+                if (sc/s.size() > h.top().score) {
+                    h.pushpop(s, sc/s.size());
+                }
+            } else if (sc/s.size() >= cutoff) {
                 h.push(s, sc/s.size());
             }
-        }
+        } 
     }
 
     return h;
 }
 
+bool isLower(string s) {
+    for (size_t i=0; i<s.size(); i++) {
+        if (!islower(s[i])) {
+            return false;
+        }
+    }
+
+    return true;
+}
 double distance(Point p1, Point p2) {
-    return sqrtf(pow(p2.x-p1.x, 2) + pow(p2.y-p1.y,  2));
+    return sqrt(pow(p2.x-p1.x, 2) + pow(p2.y-p1.y,  2));
 }
 
 double score(double distance) {
