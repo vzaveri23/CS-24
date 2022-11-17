fc92cefa6c62e4f092ccfae31e409b12ed0f611e
vzaveri23 <virajsz1123@gmail.com>
2022-11-17 13:41:49 -0800

    added member variables and functions

--- starmap/StarMap.h
+++ starmap/StarMap.h
@@ -2,6 +2,7 @@
 #define STARMAP_H
 
 #include "Star.h"
+#include "Helpers.h"
 
 #include <istream>
 #include <vector>
@@ -13,7 +14,7 @@ public:
 
 private:
     // Member Variables
-
+    KDTree *kt;
 public:
     // Constructor & Destructor
     StarMap(std::istream& stream);
@@ -23,6 +24,7 @@ public:
     std::vector<Star> find(size_t n, float x, float y, float z);
 
     // Other Member Functions
+    void findHelp(KDTree::Node *, Heap&, Star&);
 };
 
 #endif
